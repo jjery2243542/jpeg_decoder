f1de6e9626a72dd8f591cce06b51b05ff1eed981
Jerry Chou <jjery2243542@gmail.com>
2018-05-05 13:43:20 +0800

    finish reading MCU

--- decoder.cpp
+++ decoder.cpp
@@ -43,27 +43,54 @@ typedef struct JpegImage {
     unordered_map<int, int> huffman_tables[N_TYPES][N_DECODE_TREES];
     int rstn;
     int h_table_id[N_TYPES][N_COMPONENTS];
-    int block[BLOCK_SIZE];
+    int** blocks[N_COMPONENTS];
 } JpegImage;
 
 typedef struct BitReader {
-    int buffer;
+    unsigned char buffer[2];
     int remain;
+    int eof;
     BitReader() {
-        buffer = 0; remain = 0;
+        remain = 0;
+        eof = 0;
     };
     int read_bit(FILE *fp) {
         int ret;
-        unsigned char c;
+        int stop = 0;
         if (remain == 0) {
-            fread(&c, 1, 1, fp);
-            buffer = 0;
-            buffer |= (int)c;
+            fread(buffer, 1, 1, fp);
             remain += 8;
+            // special indicator
+            while (buffer[0] == 0xff && !stop) {
+                fread(buffer + 1, 1, 1, fp);
+                switch (buffer[1]) {
+                    // do nothing, ignore 00
+                    case 0x00: 
+                        stop = 1;
+                        break;
+                    case 0xd0: case 0xd1: case 0xd2: case 0xd3: 
+                    case 0xd4: case 0xd5: case 0xd6: case 0xd7:
+                        fread(buffer, 1, 1, fp);
+                        break;
+                    case 0xd9: 
+                        eof = 1;
+                        stop = 1;
+                        break;
+                    case 0xff: 
+                        break;
+                    default: 
+                        buffer[0] = buffer[1];
+                        stop = 1;
+                        break;
+                }
+            }
         }
-        ret = ((buffer >> (remain - 1)) & 1);
+        ret = ((buffer[0] >> (remain - 1)) & 1);
         remain--;
         return ret;
+    }
+    int is_eof(void) {
+        return eof;
     }
 } BitReader;
 
@@ -79,6 +106,10 @@ int ConcatBytes(auto high, auto low) {
 int ConcatInt(int high, int low) {
     return ((high << 16) | low);
 }
+
+//void dequantize(int* table, int* q_table) {
+//    
+//}
 
 void readAPP0(FILE* fp, JpegImage* ptr) {
     unsigned char buffer[2];
@@ -104,6 +135,26 @@ void readAPP0(FILE* fp, JpegImage* ptr) {
     return;
 }
 
+void toZigzagOrder(int* table, auto* buffer) {
+    int buffer_index = 0;
+    int i = 0, j = 0, di = -1, dj = 1;
+    for (int diag = 0; diag < BLOCK_SIDE + BLOCK_SIDE -1; diag++) {
+        while (i >= 0 && j >= 0 && i < BLOCK_SIDE && j < BLOCK_SIDE) {
+            //printf("%d %d\n", i, j);
+            table[i * BLOCK_SIDE + j] = (int)buffer[buffer_index];
+            buffer_index++;
+            i += di; j+= dj;
+        }
+        i -= di; j -= dj;
+        di *= -1; dj *= -1;
+        if ((i == 0 || i == BLOCK_SIDE - 1) && j < BLOCK_SIDE - 1)
+            j++;
+        else
+            i++;
+    }
+    return;
+}
+/*
 void Fill_Q_table(int* q_table, unsigned char* buffer) {
     int buffer_index = 0;
     int i = 0, j = 0, di = -1, dj = 1;
@@ -123,7 +174,7 @@ void Fill_Q_table(int* q_table, unsigned char* buffer) {
     }
     return;
 }
-
+*/
 void readDQT(FILE* fp, JpegImage* ptr) {
     unsigned char buffer[1 + BLOCK_SIZE];
     int length = 0;
@@ -140,7 +191,8 @@ void readDQT(FILE* fp, JpegImage* ptr) {
         id = (int)(buffer[0] & 0x0f);
         //printf("q_table_id=%d\n", id);
         ptr->n_q_tables = id + 1;
-        Fill_Q_table(ptr->q_table[id], buffer + 1);
+        toZigzagOrder(ptr->q_table[id], buffer + 1);
+        //Fill_Q_table(ptr->q_table[id], buffer + 1);
     }
     return;
 }
@@ -183,7 +235,7 @@ void readSOF(FILE* fp, JpegImage* ptr) {
         ptr->q_table_id[i] = (int)buffer[2];
         //printf("%d %d %d\n", ptr->subsampling[i][0], ptr->subsampling[i][1], ptr->q_table_id[i]);
     }
-    printf("Hmax=%d, Vmax=%d\n", ptr->Hmax, ptr->Vmax);
+    //printf("Hmax=%d, Vmax=%d\n", ptr->Hmax, ptr->Vmax);
 
     return;
 }
@@ -209,22 +261,22 @@ void readDHT(FILE* fp, JpegImage* ptr) {
         table_id = buffer[0] & 0xf;
         if (table_id >= N_DECODE_TREES)
             printf("huffman table id error\n");
-        printf("type=%d, table_id=%d\n", type, table_id);
+        //printf("type=%d, table_id=%d\n", type, table_id);
         auto& map = ptr->huffman_tables[type][table_id];
         for (int i = 0; i < MAX_CODE_LENGTH; i++) {
             num_leaves[i] = (int)buffer[i + 1];
-            printf("length=%d, num=%d\n", i + 1, num_leaves[i]);
+            //printf("length=%d, num=%d\n", i + 1, num_leaves[i]);
             if (num_leaves[i] > 0)
                 max_length = i + 1;
         }
         code = 0;
         for (int len = 1; len <= max_length; len++) {
-            printf("len=%d, num_leaves=%d\n", len, num_leaves[len - 1]);
+            //printf("len=%d, num_leaves=%d\n", len, num_leaves[len - 1]);
             for (int i = 0; i < num_leaves[len - 1]; i++) {
                 readBytes(fp, buffer, 1);
                 bytes_left--;
                 map[ConcatInt(len, code)] = (int)buffer[0];
-                printf("len=%d, code=%x, weights=%d\n", len, code, (int)buffer[0]);
+                //printf("len=%d, code=%x, weights=%d\n", len, code, (int)buffer[0]);
                 code++;
             }
             code = code << 1;
@@ -269,6 +321,8 @@ int translate(int weight, BitReader& BR, FILE* fp) {
     for (int len = 0; len < weight; len++) {
         bit = BR.read_bit(fp);
         //printf("bit=%d ", bit);
+        if (BR.is_eof())
+            break;
         value = (value << 1) | bit;
         if (len == 0)
             is_negative = ((bit == 0)? 1 : 0); 
@@ -285,36 +339,45 @@ int translate(int weight, BitReader& BR, FILE* fp) {
 void decodeMCU(FILE* fp, auto& dc_table, auto& ac_table, JpegImage* ptr, BitReader& BR, int* table) {
     unsigned char buffer[2];
     int count = 0, terminate = 0;
-    while (count >= BLOCK_SIZE || terminate) {
+    while (count < BLOCK_SIZE && !terminate) {
         int length = 0, code = 0, find = 0, weight = 0, translation = 0;
         int n_zeros = 0, ac_value = 0;
         auto& huffman_table = (count == 0)? dc_table: ac_table;
         int bit = 0;
-        printf("block index=%d\n", count);
+        //printf("block index=%d\n", count);
         while (!find) {
             bit = BR.read_bit(fp);
-            printf("bit=%d ", bit);
+            if (BR.is_eof()) {
+                //printf("eof detected\n");
+                return;
+            }
+            //printf("bit=%d, ", bit);
             code = (code << 1) | bit;
             length++;
-            printf("length=%d, code=%x\n", length, code);
+            //printf("length=%d, code=%x\n", length, code);
             auto got = huffman_table.find(ConcatInt(length, code));
             // code found
             if (got != huffman_table.end()) {
-                printf("gotten, code=%x\n", code);
+                //printf("gotten, code=%x\n", code);
                 weight = got->second;
                 find = 1; 
             }
         }
-        printf("weight=%d\n", weight);
+        //printf("weight=%d\n", weight);
         // AC terminate code
-        if (weight == 0) {
+        if (count > 0 && weight == 0) {
             terminate = 1;
-            continue;
+            //printf("terminated\n");
+            break;
         }
         // DC 
         if (count == 0) {
             // read weight bits of data
             translation = translate(weight, BR, fp);
+            if (BR.is_eof()) {
+                //printf("eof detected\n");
+                return;
+            }
             table[count] = translation;
             count++;
         } else {
@@ -323,25 +386,37 @@ void decodeMCU(FILE* fp, auto& dc_table, auto& ac_table, JpegImage* ptr, BitRead
             weight = weight & 0xf;
             //printf("AC: n_zeros=%d, weight=%d\n", n_zeros, weight);
             translation = translate(weight, BR, fp);
+            if (BR.is_eof()) {
+                //printf("eof detected\n");
+                return;
+            }
+            //printf("translation=%d\n", translation);
             // fill in AC value
-            for (int zero = 0; zero < n_zeros; zero++, count++) 
-                table[count] = 0; 
+            count += n_zeros;
             table[count] = translation;
             count++;
+            //if (count > BLOCK_SIZE)
+            //printf("error2, count=%d\n", count);
         }
     }
     return;
 }
 
-void readMCU(FILE* fp, JpegImage* ptr) {
+int readMCU(FILE* fp, JpegImage* ptr) {
     unsigned char buffer[BLOCK_SIZE];
-    vector<int*> tables[N_COMPONENTS];
     BitReader BR;
     int n_rows_mcu = BLOCK_SIDE * ptr->Vmax;
     int n_cols_mcu = BLOCK_SIDE * ptr->Hmax;
-    printf("height=%d, width=%d\n", ptr->height, ptr->width);
-    int n_mcu = ceil(ptr->height / n_rows_mcu) * ceil(ptr->width / n_cols_mcu); 
-    printf("n_mcu=%d\n", n_mcu);
+    //printf("height=%d, width=%d\n", ptr->height, ptr->width);
+    //printf("subsampling=%d, %d\n", ptr->Vmax, ptr->Hmax);
+    int n_mcu = (ptr->height / n_rows_mcu + 1) * (ptr->width / n_cols_mcu + 1); 
+    //printf("n_mcu=%d\n", n_mcu);
+    for (int c = 0; c < N_COMPONENTS; c++) {
+        int n_tables = n_mcu * ptr->subsampling[c][0] * ptr->subsampling[c][1];
+        ptr->blocks[c] = new int* [n_tables];
+        for (int t = 0; t < n_tables; t++) 
+            ptr->blocks[c][t] = new int [BLOCK_SIZE](); 
+    }
     for (int mcu = 0; mcu < n_mcu; mcu++) {
         //printf("mcu_index=%d\n", mcu);
         for (int c = 0; c < N_COMPONENTS; c++) {
@@ -351,17 +426,21 @@ void readMCU(FILE* fp, JpegImage* ptr) {
             auto& dc_table = ptr->huffman_tables[DC][dc_table_id];
             auto& ac_table = ptr->huffman_tables[AC][ac_table_id];
             int n_tables = ptr->subsampling[c][0] * ptr->subsampling[c][1];
-            int* table = new int(BLOCK_SIZE);
             int previous_dc = 0;
+            int buffer_table[BLOCK_SIZE];
             for (int t = 0; t < n_tables; t++) {
-                 decodeMCU(fp, dc_table, ac_table, ptr, BR, table);
-                 table[0] += previous_dc;
-                 previous_dc = table[0];
-                 tables[c].push_back(table);
+                decodeMCU(fp, dc_table, ac_table, ptr, BR, buffer_table);
+                toZigzagOrder(ptr->blocks[c][mcu * n_tables + t], buffer_table);
+                ptr->blocks[c][mcu * n_tables + t][0] += previous_dc;
+                previous_dc = ptr->blocks[c][mcu * n_tables + t][0];
+                if (BR.is_eof()) {
+                    //printf("eof detected\n");
+                    return 1;
+                }
             } 
         }
     }
-    return;
+    return 0;
 }
 
 JpegImage* ReadJpeg(FILE* fp) {
@@ -376,47 +455,37 @@ JpegImage* ReadJpeg(FILE* fp) {
     while (!end) {
         readBytes(fp, buffer, 2);
         switch(buffer[1]) {
-            case MarkerAPP0: {
-                printf("first\n");
+            case MarkerAPP0: 
+                //printf("first\n");
                 readAPP0(fp, ImagePtr);
                 break;
-            }
-            case MarkerDQT: {
-                printf("second\n");
+            case MarkerDQT: 
+                //printf("second\n");
                 readDQT(fp, ImagePtr);
                 break;
-            }
-            case MarkerSOF: {
-                printf("third\n");
+            case MarkerSOF: 
+                //printf("third\n");
                 readSOF(fp, ImagePtr);
                 break;
-            }
-            case MarkerDHT: {
-                printf("fourth\n");
+            case MarkerDHT: 
+                //printf("fourth\n");
                 readDHT(fp, ImagePtr);
                 break;
-            }
-            case MarkerDRI: {
-                printf("fifth\n");
+            case MarkerDRI: 
+                //printf("fifth\n");
                 readDRI(fp, ImagePtr);
                 break;
-            }
-            case MarkerSOS: {
-                printf("sixth\n");
+            case MarkerSOS: 
+                //printf("sixth\n");
                 readSOS(fp, ImagePtr);
-                readMCU(fp, ImagePtr);
+                end = readMCU(fp, ImagePtr);
+                break;
+            case MarkerEOI: 
+                //printf("end\n");
                 end = 1;
                 break;
-            }
-            case MarkerEOI: {
-                printf("end\n");
-                end = 1;
-                break;
-            }
         }
     }
-    readBytes(fp, buffer, 1);
-    printf("%x", buffer[0]);
     return ImagePtr;
 }
 
